2003c63da4f6ca4d7960fc5e24f0f8f1c2af7960
Rawphs <raphaelaprotasio@gmail.com>
2018-10-30 09:25:21 +0100

    feat(website): implement "What does it contain?" section

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -1,18 +1,8 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 const React = require('react');
-
 const CompLibrary = require('../../core/CompLibrary.js');
-
 const MarkdownBlock = CompLibrary.MarkdownBlock; /* Used to read markdown */
 const Container = CompLibrary.Container;
 const GridBlock = CompLibrary.GridBlock;
-
 const siteConfig = require(`${process.cwd()}/siteConfig.js`);
 
 function imgUrl(img) {
@@ -27,11 +17,53 @@ function pageUrl(page, language) {
   return siteConfig.baseUrl + (language ? `${language}/` : '') + page;
 }
 
+const whyStix = [
+  {
+    content: 'Easily split up and reuse parts of your domain. Simply plug in a module like user or shopping cart; configure it and get going. Creating and using modules is easy. They\'re just regular directories. No magic. No directory scanning, no prefixes, just your own code.',
+    image: imgUrl('module.svg'),
+    imageAlign: 'top',
+    title: 'Module Based',
+  },
+  {
+    content: 'Written with and for TypeScript projects. Although it does work with flow and ES6 based projects, the focus is on TypeScript. Some of the reasons stix is so fast can also be attributed to its use of new features and lazy nature of service loading.',
+    image: imgUrl('fast.svg'),
+    imageAlign: 'top',
+    title: 'Modern',
+  },
+  {
+    content: 'Proven to work and scale. This includes inversion of control, service managers and injectors for controllers and commands making it easy to create highly reusable and configurable modules for your API, or the CLI.',
+    image: imgUrl('pattern.svg'),
+    imageAlign: 'top',
+    title: 'Design Patterns',
+  },
+];
+
+const stixContent = [
+  {
+    content: 'Stix comes with a command line environment. Just as you can create API endpoints, you can create CLI commands. Using stix-cli you automatically get help output, as well as support for autocomplete on the cli, or as some call it "tabtab support".',
+    image: imgUrl('cli.svg'),
+    imageAlign: 'top',
+    title: 'Command-line',
+  },
+  {
+    content: 'We all love documentation. We all hate writing it. Especially swagger documentation. That\'s why stix-swagger (for real, we nailed naming these modules) automatically generates your swagger docs for you. Installing it will directly document your endpoints for you.',
+    image: imgUrl('api.svg'),
+    imageAlign: 'top',
+    title: 'API Documentation',
+  },
+  {
+    content: 'Feared by some, loved by many. Code generators. Quickly scaffold your boilerplate using generators. Initialize a new module, add a controller, create a new entity. All without writing a single line of code. The code generated is basic and simple and thus there\'s no magic. Scaffold and code away.',
+    image: imgUrl('generator.svg'),
+    imageAlign: 'top',
+    title: 'Code Generators',
+  },
+];
+
 class Button extends React.Component {
   render() {
     return (
       <div className="pluginWrapper buttonWrapper">
-        <a className="button" href={this.props.href} target={this.props.target}>
+        <a className="button homeButton" href={this.props.href} target={this.props.target}>
           {this.props.children}
         </a>
       </div>
@@ -97,80 +129,13 @@ const Block = props => (
   </Container>
 );
 
-const Features = () => (
+const Features = (props) => (
  <React.Fragment>
-   <h3 className="sectionTitle">Why Stix?</h3>
+   <h3 className="sectionTitle">{props.title}</h3>
    <Block layout="threeColumn" padding={['bottom']}>
-     {[
-       {
-         content: 'Easily split up and reuse parts of your domain. Simply plug in a module like user or shopping cart; configure it and get going. Creating and using modules is easy. They\'re just regular directories. No magic. No directory scanning, no prefixes, just your own code.',
-         image: imgUrl('module.svg'),
-         imageAlign: 'top',
-         title: 'Module Based',
-       },
-       {
-         content: 'Written with and for TypeScript projects. Although it does work with flow and ES6 based projects, the focus is on TypeScript. Some of the reasons stix is so fast can also be attributed to its use of new features and lazy nature of service loading.',
-         image: imgUrl('fast.svg'),
-         imageAlign: 'top',
-         title: 'Modern',
-       },
-       {
-         content: 'Proven to work and scale. This includes inversion of control, service managers and injectors for controllers and commands making it easy to create highly reusable and configurable modules for your API, or the CLI.',
-         image: imgUrl('pattern.svg'),
-         imageAlign: 'top',
-         title: 'Design Patterns',
-       },
-     ]}
+     {props.content}
    </Block>
  </React.Fragment>
-);
-
-const FeatureCallout = () => (
-  <div
-    className="productShowcaseSection paddingBottom"
-    style={{textAlign: 'center'}}>
-    <h2>Feature Callout</h2>
-    <MarkdownBlock>These are features of this project</MarkdownBlock>
-  </div>
-);
-
-const StixCli = () => (
-  <Block background="light" className="featureBlock">
-    {[
-      {
-        content: 'Stix comes with a command line environment. Just as you can create API endpoints, you can create CLI commands. Using stix-cli you automatically get help output, as well as support for autocomplete on the cli, or as some call it "tabtab support".',
-        image: imgUrl('stix_cli.svg'),
-        imageAlign: 'right',
-          title: 'Command-line',
-      },
-    ]}
-  </Block>
-);
-
-const TryOut = () => (
-  <Block id="try">
-    {[
-      {
-        content: 'Feared by some, loved by many. Code generators. Quickly scaffold your boilerplate using generators. Initialize a new module, add a controller, create a new entity. All without writing a single line of code. The code generated is basic and simple and thus there\'s no magic. Scaffold and code away.',
-        image: imgUrl('docusaurus.svg'),
-        imageAlign: 'left',
-        title: 'Try it Out',
-      },
-    ]}
-  </Block>
-);
-
-const Description = () => (
-  <Block background="dark">
-    {[
-      {
-        content: 'This is another description of how this project is useful',
-        image: imgUrl('docusaurus.svg'),
-        imageAlign: 'right',
-        title: 'Description',
-      },
-    ]}
-  </Block>
 );
 
 const Showcase = props => {
@@ -207,7 +172,8 @@ class Index extends React.Component {
         <HalfCube />
         <HomeSplash language={language} />
         <div className="mainContainer">
-          <Features />
+          <Features content={whyStix} title="Why Stix?" />
+          <Features content={stixContent} title="What does it contain?" />
           {/*<FeatureCallout />*/}
           {/*<StixCli />*/}
           {/*<TryOut />*/}
